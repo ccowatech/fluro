2cd4b53d0e9b399a7bfb57704e05ad53a831711a
Sam Rae <149111+samrae@users.noreply.github.com>
2022-04-02 13:08:14 +0800

    Reaction: Send link to Square invoice - Further updates to code style

--- Reactions/Send link to Square invoice/05-Retrieve data.js
+++ Reactions/Send link to Square invoice/05-Retrieve data.js
@@ -2,37 +2,42 @@
 Retrieve the required data
 */
 
-const interaction = input.interaction;
+// Load packages
+const _ = require('lodash');
+
+// Get input we need
+const { interaction, inputType } = input;
 
 // Check that a Transaction and a Contact are attached to this Interaction.
-const transactionExists = interaction.hasOwnProperty('transactions');
-const contactExists = interaction.hasOwnProperty('contacts');
-
-// If there's no transaction or contact, stop the Reaction
-if (transactionExists === false || contactExists === false) {
+if (!_.has(interaction, 'transactions') || !_.has(interaction, 'contacts')) {
+    // If there's no transaction or contact, stop the Reaction
     return done(null, 'STOP');
 }
 
 // Find all the data needed for the next steps
-const inputType = input.inputType;
-const formTitle = interaction.title;
-const formRealm = interaction.realms[0];
-const receiptUrl = interaction.transactions[0].transactionData.payment.receiptUrl;
-const statementDescription = interaction.transactions[0].transactionData.payment.cardDetails.statementDescription;
+const {
+    receiptUrl,
+    cardDetails: { statementDescription }
+} = interaction.transactions[0].transactionData.payment;
+
+const { title: formTitle, realms: formRealms } = interaction;
+const formRealm = formRealms[0];
+
 const interactionLink = `https://app.fluro.io/list/interaction/${interaction.definition}/${input.interaction._id}/edit`;
 const transactionLink = `https://app.fluro.io/list/transaction/${interaction.transactions[0]._id}/edit`;
 const contact = interaction.contacts[0];
 
 // Format input nicely for next steps
-input = {};
-input.formTitle = formTitle;
-input.formRealm = formRealm;
-input.receiptUrl = receiptUrl;
-input.statementDescription = statementDescription;
-input.interactionLink = interactionLink;
-input.transactionLink = transactionLink;
-input.contact = contact;
-input.inputType = inputType;
+input = {
+    formTitle,
+    formRealm,
+    receiptUrl,
+    statementDescription,
+    interactionLink,
+    transactionLink,
+    contact,
+    inputType
+};
 
 // Finish the action and send the input to the next action
 return done(null, input);
