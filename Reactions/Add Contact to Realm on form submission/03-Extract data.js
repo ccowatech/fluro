f33f9e02bf57592018595ee9e612713533bf757b
Sam Rae <149111+samrae@users.noreply.github.com>
2022-04-02 22:47:11 +0800

    Reaction: Add Contact to Realm on form submission
    - FIXED: Now works for single-contact forms

--- Reactions/Add Contact to Realm on form submission/03-Extract data.js
+++ Reactions/Add Contact to Realm on form submission/03-Extract data.js
@@ -2,9 +2,27 @@
 Extract data we need
 */
 
+// Load packages
+const _ = require('lodash');
+
 // Get data from input
 const interactionRealms = input.item.realms;
-const contacts = input.data.submittedData.data.contact;
+const inputContacts = input.data.submittedData.data.contact;
+
+let contacts = [];
+
+// If single contact object
+if (_.has(inputContacts, '_id')) {
+    contacts.push(inputContacts);
+
+// If array of contact objects
+} else if (_.isArray(inputContacts)) {
+    contacts = inputContacts;
+
+// Else seems like there are no contacts attached. Stop the Reaction.
+} else {
+    return done(null, 'STOP');
+}
 
 // Replace input with only the data we need
 input = { interactionRealms, contacts };
